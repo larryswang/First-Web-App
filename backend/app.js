5ad75a8151dd313b3c94e60878a4b14bd92fdcc3
Shiyu Wang <shiyuwang@Jennifers-MacBook-Pro-3.local>
2018-09-20 23:32:01 -0400

    add app code

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,16 @@ const express = require('express')
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware')
-  next();
-});
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id : 'fadafdsafdsa', title: 'First server-side post', contest: "this is coming from post"},
+    { id : 'jhbiuhblkb', title: 'Second server-side post', contest: "this is coming from post!"}
+  ];
+  res.status(200).json({
+    message: 'Posts fetch successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
