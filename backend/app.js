02bd109b2a47855accd0134f9617b7c89dc8cecb
Shiyu Wang <shiyuwang@Jennifers-MacBook-Pro-3.local>
2018-09-22 14:15:22 -0400

    finish database part

--- backend/app.js
+++ backend/app.js
@@ -36,9 +36,11 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: "Post added sucessfully"
+  post.save().then(result => {
+    res.status(201).json({
+      message: "Post added sucessfully",
+      postId: result._id
+    });
   });
 });
 
@@ -55,8 +57,9 @@ app.get("/api/posts", (req, res, next) => {
 app.delete("/api/posts/:id", (req, res, next) => {
   Post.deleteOne( {_id: req.params.id}).then(result => {
     console.log(result);
+    res.status(200).json({ message: 'Post deleted!'});
   });
-  res.status(200).json({ message: 'Post deleted!'});
+
 })
 
 module.exports = app;
