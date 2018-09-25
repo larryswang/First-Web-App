be7da3273bfcc1ec3c73b67e47a69f55c8fb9b1d
Shiyu Wang <shiyuwang@Jennifers-MacBook-Pro-3.local>
2018-09-24 23:59:16 -0400

    edit button works

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, DELETE, PUT, OPTIONS"
   );
   next();
 });
@@ -52,6 +52,18 @@ app.get("/api/posts", (req, res, next) => {
         posts: documents
       });
     });
+});
+
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post ({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Update successful1" });
+  })
 });
 
 app.delete("/api/posts/:id", (req, res, next) => {
