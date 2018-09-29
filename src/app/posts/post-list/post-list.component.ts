d6e189cd033f8ae069d24be7da8d9f05c7e93d4d
Shiyu Wang <shiyuwang@Jennifers-MacBook-Pro-3.local>
2018-09-29 16:38:00 -0400

    add login module

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
-import { Post } from "../post.model";
-import { PostsService } from "../posts.service";
-import { PageEvent } from "@angular/material";
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { PageEvent } from '@angular/material';
 
 @Component({
-  selector: "app-post-list",
-  templateUrl: "./post-list.component.html",
-  styleUrls: ["./post-list.component.css"]
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
