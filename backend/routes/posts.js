7f3cd79a610b60ecdb6285592dbcbb832bac51be
Shiyu Wang <shiyuwang@Jennifers-MacBook-Pro-3.local>
2018-10-02 22:48:50 -0400

    finish error handlings

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -51,6 +51,11 @@ router.post(
           id: createdPost._id
         }
       });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Creating a post failed!"
+      })
     });
   }
 );
@@ -72,12 +77,20 @@ router.put(
       imagePath: imagePath,
       creator: req.userData.userId
     });
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
+    Post.updateOne(
+      { _id: req.params.id, creator: req.userData.userId },
+      post
+      ).then(result => {
       if (result.nModified > 0) {
         res.status(200).json({ message: "Update successful!"});
       } else {
       res.status(401).json({ message: "Not authorized!" });
       }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Couldn't update post!"
+      })
     });
   }
 );
@@ -101,6 +114,10 @@ router.get("", (req, res, next) => {
         posts: fetchedPosts,
         maxPosts: count
       });
+    }).catch(error => {
+      res.status(500).json({
+        message: "Fetching posts failed!"
+      });
     });
 });
 
@@ -111,6 +128,10 @@ router.get("/:id", (req, res, next) => {
     } else {
       res.status(404).json({ message: "Post not found!" });
     }
+  }).catch(error => {
+    res.status(500).json({
+      message: "Fetching posts failed!"
+    });
   });
 });
 
@@ -121,6 +142,10 @@ router.delete("/:id", checkAuth, (req, res, next) => {
     } else {
     res.status(401).json({ message: "Not authorized!" });
     }
+  }).catch(error => {
+    res.status(500).json({
+      message: "Fetching posts failed!"
+    });
   });
 });
 
